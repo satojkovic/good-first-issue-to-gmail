7e07ce4268a46c7ad725c08bfdcadb354a695d49
satojkovic <satojkovic@gmail.com>
2024-07-05 23:54:28 +0900

    Add python script for searching good first issues

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+
+
+def search_github_issues(label, state, language):
+    url = "https://api.github.com/search/issues?"
+    query = f'q=label:"{label}"+is:{state}+language:{language}'
+
+    url = url + query
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to retrieve data: {response.status_code}")
+        return None
+
+
+if __name__ == "__main__":
+    label = "good first issue"
+    state = "open"
+    language = "python"
+
+    result = search_github_issues(label, state, language)
+
+    if result:
+        issues = result.get("items", [])
+        for issue in issues:
+            print(f"Title: {issue['title']}")
+            print(f"URL: {issue['html_url']}")
+            print(f"Repository: {issue['repository_url']}")
+            print("-" * 80)
